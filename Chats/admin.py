add6d6879b55db141bdf58fb3eed08e1bf622c60
Keval Vasani <kevalv.tagline@gmail.com>
2024-07-04 16:48:54 +0530

    Added Models

--- Chats/admin.py
+++ Chats/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 from .models import Conversation, Message
 
+
+# Register your models here.
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    """
+    This class displays user data in the admin panel.
+    """
+
+    list_display = ["id", "conversation_name"]
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    """
+    This class displays user data in the admin panel.
+    """
+
+    list_display = ["id", "conversation", "sender", "text"]
--- Posts/admin.py
+++ Posts/admin.py
@@ -1,1 +1,48 @@
 from django.contrib import admin
+from .models import Post,PostImageVideo,Like,Comment,Friendship
+
+
+# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """
+    This class displays user data in the admin panel.
+    """
+
+    list_display = ["id", "user", "content"]
+
+
+@admin.register(PostImageVideo)
+class PostImageVideoAdmin(admin.ModelAdmin):
+    """
+    This class displays user data in the admin panel.
+    """
+
+    list_display = ["id", "post"]
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    """
+    This class displays user data in the admin panel.
+    """
+
+    list_display = ["id", "user", "post"]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    This class displays user data in the admin panel.
+    """
+
+    list_display = ["id", "user", "post", "content"]
+
+
+@admin.register(Friendship)
+class FriendshipAdmin(admin.ModelAdmin):
+    """
+    This class displays user data in the admin panel.
+    """
+
+    list_display = ["id", "from_user", "to_user", "is_accepted"]
